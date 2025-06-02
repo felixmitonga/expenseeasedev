79cf7da159faf608e3f303e9d47a47edec5cf7c8
Felix Mitonga <63664755+felixmitonga@users.noreply.github.com>
2025-06-02 21:13:30 +0200

    interface tweak before run build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './Dashboard';
+
 import ScanReceipt from './ScanReceipt';
 import Reports from './Reports';
 import AddExpense from './AddExpense';
@@ -10,12 +10,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
         <Route path="/scan" element={<ScanReceipt />} />
         <Route path="/reports" element={<Reports />} />
+        <Route path="/AddExpense" element={<AddExpense />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
